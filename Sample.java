af9c531bd76bba3d5b5567a8763da1a631a9cf8f
DineshBandi <100129755+Dinesh1215@users.noreply.github.com>
2022-05-04 17:49:00 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,12 @@
+//  static blocks sample program
+class Sample
+  {
+    static
+    {
+      System.out.println("Its Static Block");
+    }
+    public static void main(String[] args)
+    {
+      System.out.println("Its Main Method");
+    }
+  }
---
+++ exception_handling.java
@@ -0,0 +1,28 @@
+//Static Method
+
+class exception_handling 
+{
+
+       public static void main(String args[]){
+
+            try{
+
+                int i, sum;
+
+                sum = 10;
+
+                for (i = -1; i < 3 ;++i)
+
+                    sum = (sum / i);
+
+            }
+
+            catch(ArithmeticException e) {
+
+        	System.out.print("0");} 
+
+            System.out.print(sum);
+
+      }
+
+    }
